4dccf81e082ce79ca914365ef2df711f858cdfd0
Gabriella Benevides <gabriellabenevides22@gmail.com>
2023-10-21 16:55:51 -0300

    first commit

---
+++ Sound/Program.cs
@@ -0,0 +1,24 @@
+﻿Banda TheWeeknd = new Banda("The Weeknd");
+
+Album albumDoTheWeeknd = new Album("After Hours");
+
+Musica musica1 = new Musica(TheWeeknd, "Save your tears")
+{
+    Duracao = 215,
+    Disponivel = true,
+};
+
+Musica musica2 = new Musica(TheWeeknd, "In your eyes")
+{
+    Duracao = 237,
+    Disponivel = false,
+};
+
+albumDoTheWeeknd.AdicionarMusica(musica1);
+albumDoTheWeeknd.AdicionarMusica(musica2);
+TheWeeknd.AdicionarAlbum(albumDoTheWeeknd);
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
+albumDoTheWeeknd.ExibirMusicasDoAlbum();
+TheWeeknd.ExibirDiscografia();
